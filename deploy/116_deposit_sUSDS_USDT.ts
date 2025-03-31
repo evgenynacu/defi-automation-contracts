bdfee7656204987ce3e8bf27ae4963f64e77189f
Eugene Nacu <evgeny.nacu@gmail.com>
2025-03-31 11:18:26 +0100

    fix: new deposit

--- deploy/116_deposit_sUSDS_USDT.ts
+++ deploy/116_deposit_sUSDS_USDT.ts
@@ -5,7 +5,7 @@ import { depositToMorpho } from "./deposit-to-morpho"
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	console.log(`deploying contracts on network ${hre.network.name}`)
 
-	await depositToMorpho(hre, "0xb5b0ff0fccf16dff5bef6d2d001d60f5c4ab49df1020a01073d3ad635c80e8d5", 1000639387n, 8)
+	await depositToMorpho(hre, "0xb5b0ff0fccf16dff5bef6d2d001d60f5c4ab49df1020a01073d3ad635c80e8d5", 10418466540n, 8)
 }
 
 // noinspection JSUnusedGlobalSymbols
--- deploy/verify-allowance.ts
+++ deploy/verify-allowance.ts
@@ -1,4 +1,5 @@
 import { ethers } from "hardhat"
+import { USDT_ADDRESS } from "./addresses"
 
 export async function verifyAllowance(token: string, amount: bigint, vault: string) {
 	const [signer] = await ethers.getSigners()
@@ -7,6 +8,10 @@ export async function verifyAllowance(token: string, amount: bigint, vault: stri
 	const allowance = await baseToken.allowance(signer.address, vault)
 	if (allowance < amount) {
 		console.log("Allowing to spend base token")
+		if (token === USDT_ADDRESS) {
+			const tx = await baseToken.approve(vault, 0)
+			await tx.wait()
+		}
 		const tx = await baseToken.approve(vault, ethers.MaxUint256)
 		await tx.wait()
 	}
