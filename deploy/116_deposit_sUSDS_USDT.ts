af141a30a2d15b41a610a078f3829de3e150305c
Eugene Nacu <evgeny.nacu@gmail.com>
2025-04-21 12:13:02 +0100

    chore: deposit

--- deploy/116_deposit_sUSDS_USDT.ts
+++ deploy/116_deposit_sUSDS_USDT.ts
@@ -6,7 +6,7 @@ import { sendOrEstimate } from "./send-or-estimate"
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	console.log(`deploying contracts on network ${hre.network.name}`)
 
-	await sendOrEstimate(hre, ex => depositToMorpho(ex, "0xb5b0ff0fccf16dff5bef6d2d001d60f5c4ab49df1020a01073d3ad635c80e8d5", 1000000000n, 7))
+	await sendOrEstimate(hre, ex => depositToMorpho(ex, "0xb5b0ff0fccf16dff5bef6d2d001d60f5c4ab49df1020a01073d3ad635c80e8d5", 11274144489n, 10))
 }
 
 // noinspection JSUnusedGlobalSymbols
