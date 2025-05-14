42e020eacda1b884fbbb1f988b8ab3d3ff070e88
Eugene Nacu <evgeny.nacu@gmail.com>
2025-05-14 09:10:32 +0100

    feat: read position

--- common/types.ts
+++ common/types.ts
@@ -1,1 +1,15 @@
 export type address = `0x${string}`
+
+export function toAddress(hex: string): address {
+	if (hex.startsWith("0x")) {
+		return hex as address
+	}
+  throw new Error("Invalid address " + hex)
+}
+
+export function toHex(possibly: string): `0x${string}` {
+	if (possibly.startsWith("0x")) {
+		return possibly as `0x${string}`
+	}
+	throw new Error("Invalid hex " + possibly)
+}
--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import express, { Application } from "express"
 import cors from "cors"
 import { createContext } from "../context"
+import { toAddress, toHex } from "../common/types"
 
 dotenv.config();
 
@@ -19,12 +20,12 @@ createContext().then(({ dataService }) => {
 		res.status(200).json({ status: "OK" })
 	})
 
-	app.get("/results/eUSDe", async (_, res) => {
+	app.get("/results/morpho/:from/:vault/:marketId", async (req, res) => {
 		const data = await dataService.getData({
 			type: "morpho-withdraw",
-			from: "0xEbca6F665A80466f410B3c2FD5a1696eDB664A42",
-			vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
-			marketId: "0xae4571cdcad4191b9a59d1bb27a10a1b05c92c84fe423e4886d5781a30a9c8f1"
+			from: toAddress(req.params.from),
+			vault: toAddress(req.params.vault),
+			marketId: toHex(req.params.marketId)
 		})
 
 		res.status(200).json(data)
