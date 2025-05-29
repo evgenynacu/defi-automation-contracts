784f061c1bdce801c04418d2ba72a99e06e89608
Eugene Nacu <evgeny.nacu@gmail.com>
2025-05-29 14:09:02 +0100

    fix: remove cron jobs

--- cron/index.ts
+++ cron/index.ts
@@ -13,16 +13,6 @@ async function runJobs() {
 	cron.schedule('* * * * *', () => {
 		console.log("Running cron job")
 
-		//PT-DAI <-> eUSDe
-		logAsync(
-			syncService.syncData({
-				type: "morpho-withdraw",
-				from: "0xEbca6F665A80466f410B3c2FD5a1696eDB664A42",
-				vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
-				marketId: "0xae4571cdcad4191b9a59d1bb27a10a1b05c92c84fe423e4886d5781a30a9c8f1"
-			})
-		)
-
 		//rUSD
 		logAsync(
 			syncService.syncData({
@@ -40,16 +30,6 @@ async function runJobs() {
 				from: "0x21F1359b6DD3392d3DC567d005d83B6d017CC60D",
 				vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
 				marketId: "0xbfed072faee09b963949defcdb91094465c34c6c62d798b906274ef3563c9cac"
-			})
-		)
-
-		//PT-DAI <-> sUSDe
-		logAsync(
-			syncService.syncData({
-				type: "morpho-withdraw",
-				from: "0xEbca6F665A80466f410B3c2FD5a1696eDB664A42",
-				vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
-				marketId: "0x407d8c123443d362ffdfe73208068ef158a21d1a44a988c9acc23a51bade7905"
 			})
 		)
 
