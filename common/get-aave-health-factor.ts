2333f33a574fcf6d5ec88c327f7be62749acbd18
Eugene Nacu <evgeny.nacu@gmail.com>
2025-06-02 18:24:14 +0100

    fix: correct wallet hf

--- common/get-aave-health-factor.ts
+++ common/get-aave-health-factor.ts
@@ -8,6 +8,6 @@ export async function getAaveHealthFactor(runner: ContractRunner, wallet: addres
 	const data = await pool.getUserAccountData(wallet)
 	return {
 		id: "aave-hf-" + wallet,
-		result: Number(1000000n * data.healthFactor) / 1000000,
+		result: Number(1000000n * data.healthFactor / 10n ** 18n) / 1000000,
 	}
 }
