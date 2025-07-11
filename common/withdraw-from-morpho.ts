2d325ee2d2cf5b5045502a731d335ff075e99ece
Eugene Nacu <evgeny.nacu@gmail.com>
2025-07-11 15:28:06 +0100

    fix: withdraw-from-morpho

--- common/withdraw-from-morpho.ts
+++ common/withdraw-from-morpho.ts
@@ -80,23 +80,38 @@ export async function withdrawFromMorpho<T>(ex: StrategyExecutor<T>, marketId: s
 
 async function getTotalBorrowAssets(ex: StrategyExecutor<any>, marketId: string, loanToken: string) {
 	const calc = createCalculateExecutor(ex.runner, "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240", "0xEbca6F665A80466f410B3c2FD5a1696eDB664A42")
-	const res = await calc.execute([
-		{
-			type: "erc20-transfer-from-caller",
-			token: usdc,
-			amount: 100000n,
-		},
-		{
-			type: "swap",
-			from: usdc,
-			to: loanToken,
-			amount: 100000n,
-			preferred: ["kyberswap", "openocean", "velora"]
-		},
-		{
-			type: "morpho-read-total-borrow-assets",
-			marketId: marketId,
-		},
-	])
-	return res.result
+	if (loanToken.toLowerCase() === usdc.toLowerCase()) {
+		const res = await calc.execute([
+			{
+				type: "erc20-transfer-from-caller",
+				token: usdc,
+				amount: 100000n,
+			},
+			{
+				type: "morpho-read-total-borrow-assets",
+				marketId: marketId,
+			},
+		])
+		return res.result
+	} else {
+		const res = await calc.execute([
+			{
+				type: "erc20-transfer-from-caller",
+				token: usdc,
+				amount: 100000n,
+			},
+			{
+				type: "swap",
+				from: usdc,
+				to: loanToken,
+				amount: 100000n,
+				preferred: ["kyberswap", "openocean", "velora"]
+			},
+			{
+				type: "morpho-read-total-borrow-assets",
+				marketId: marketId,
+			},
+		])
+		return res.result
+	}
 }
