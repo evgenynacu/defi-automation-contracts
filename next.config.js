56e7c1b920d60bb13371b07d07829526e6bf1687
Eugene Nacu <evgeny.nacu@gmail.com>
2025-07-16 13:45:01 +0100

    chore: minor change

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 module.exports = {
-    async rewrites() {
+    rewrites: async function() {
         return [
             {
                 source: '/api/:path*',
-                destination: `${process.env.API_BASE_URL || 'http://localhost:8080'}/api/:path*`,
+                destination: `${process.env.API_BASE_URL}/api/:path*`,
             },
         ];
     },
