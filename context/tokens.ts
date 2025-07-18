e77888c2f06408d6171bf437e66da1351cd65c0a
Eugene Nacu <evgeny.nacu@gmail.com>
2025-07-18 16:09:57 +0100

    chore: new positions

--- context/tokens.ts
+++ context/tokens.ts
@@ -1,7 +1,7 @@
 import { address } from "../common/types"
 import {
 	DAI_ADDRESS,
-	PT_eUSDe_AUG, SDAI_ADDRESS, sUSDe_ADDRESS, usdc,
+	PT_eUSDe_AUG, PT_sUSDe_SEP, SDAI_ADDRESS, sUSDe_ADDRESS, usdc,
 	USDe_ADDRESS,
 	USDT_ADDRESS,
 	WEETH_ADDRESS,
@@ -13,6 +13,7 @@ export const tokens: Record<address, string> = {
 	[usdc]: "USDC",
 	[USDT_ADDRESS]: "USDT",
 	[PT_eUSDe_AUG]: "PT-eUSDe-AUG25",
+	[PT_sUSDe_SEP]: "PT-sUSDe-SEP25",
 	[WETH_ADDRESS]: "WETH",
 	[WEETH_ADDRESS]: "weETH",
 	[WSTETH_ADDRESS]: "wstETH",
