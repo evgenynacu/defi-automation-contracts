7e1481a3d743b0fa38174336b49546a94b40b046
Eugene Nacu <evgeny.nacu@gmail.com>
2025-09-15 11:18:36 +0100

    fix: etherscan-verify v2 api

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config"
 import "@nomicfoundation/hardhat-toolbox"
 import "hardhat-deploy"
-import "hardhat-deploy-ethers"
 import { config as dotenvConfig } from "dotenv"
 import { resolve } from "path"
 import { NetworksUserConfig } from "hardhat/types"
