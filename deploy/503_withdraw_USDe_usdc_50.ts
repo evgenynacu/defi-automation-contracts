21684d13aac36487e93b3c68ec0b0bf90b0d6460
Eugene Nacu <evgeny.nacu@gmail.com>
2025-10-16 13:24:48 +0100

    fix: warn

--- deploy/503_withdraw_USDe_usdc_50.ts
+++ deploy/503_withdraw_USDe_usdc_50.ts
@@ -1,7 +1,7 @@
 import {HardhatRuntimeEnvironment} from 'hardhat/types'
 import {DeployFunction} from 'hardhat-deploy/types'
 import {withdrawFromAave} from "../common/withdraw-from-aave"
-import {sUSDe_ADDRESS, USDC, USDe_ADDRESS} from "../common/addresses"
+import {USDC, USDe_ADDRESS} from "../common/addresses"
 import {sendOrEstimate} from "./send-or-estimate"
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
