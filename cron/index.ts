cabc8e9fe3abefecaa5ff588b1320cb7c7bf1cf3
Eugene Nacu <evgeny.nacu@gmail.com>
2026-01-11 09:25:11 +0000

    chore: positions

--- cron/index.ts
+++ cron/index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import {createContext} from "../context"
 import {runMigrations} from "../context/db/run-migrations"
 import {logAsync} from "../common/log-async"
-import {PT_sUSDe_FEB26, sUSDe_ADDRESS, USDe_ADDRESS} from "../common/addresses"
+import {PT_sUSDe_FEB26, sUSDe_ADDRESS, sUSDS, USDe_ADDRESS, USDT_ADDRESS} from "../common/addresses"
 import {updateJobs} from "../context/db/update-jobs"
 import {Pool} from "pg";
 import {SyncService} from "../context/service/sync-service";
@@ -114,6 +114,24 @@ async function runJobs() {
 				amount: 100000000000000000000000n,
 			}),
 			"checking sUSDe-USDe rate"
+		)
+		logAsync(
+			syncService.syncData({
+				type: "swap-rate",
+				fromToken: USDT_ADDRESS,
+				toToken: sUSDS,
+				amount: 100000000000n,
+			}),
+			"checking USDT-sUSDS rate"
+		)
+		logAsync(
+			syncService.syncData({
+				type: "swap-rate",
+				fromToken: sUSDS,
+				toToken: USDT_ADDRESS,
+				amount: 100000000000000000000000n,
+			}),
+			"checking USDT-sUSDS rate"
 		)
 
 		// logAsync(
--- deploy/156_deposit_PT-thBILL_USDC_morpho.ts
+++ deploy/156_deposit_PT-thBILL_USDC_morpho.ts
@@ -9,7 +9,7 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 	await sendOrEstimate(
 		hre,
 		ex =>
-			depositToMorpho(ex, "0x6c831dcc45a7c0af00b751da651bd874b96653c587615d11aafade7b357c4b43", 1658000000n, 8)
+			depositToMorpho(ex, "0x6c831dcc45a7c0af00b751da651bd874b96653c587615d11aafade7b357c4b43", 27390000000n, 8)
 	)
 }
 
@@ -18,3 +18,5 @@ export default func
 func.tags = ['deposit-PT-thBILL-USDC-morpho']
 
 // 13413.387213 - 1658 * 7 * (1 + 0.057 * 43 / 365) - 1658 = 71 = 36%
+// 80883.407022 - 10000 * 7 * (1 + 0.057 * 43 / 365) - 10000 = 413 = 35%
+// 221444.759248 - 27390 * 7 * (1 + 0.057 * 43 / 365) - 27390 = 1037 = 32%
