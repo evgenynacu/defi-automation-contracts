f0982415f7e8ffae88f322d5100fdcd82937b65e
Eugene Nacu <evgeny.nacu@gmail.com>
2026-02-05 14:44:55 +0000

    chore: positions

---
+++ deploy/408_withdraw_sUSDe.ts
@@ -0,0 +1,15 @@
+import {HardhatRuntimeEnvironment} from 'hardhat/types'
+import {DeployFunction} from 'hardhat-deploy/types'
+import {withdrawFromAave} from "../common/withdraw-from-aave"
+import {sUSDe_ADDRESS, USDC, USDe_ADDRESS} from "../common/addresses"
+import {sendOrEstimate} from "./send-or-estimate"
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+	console.log(`withdrawing on network ${hre.network.name}`)
+
+	await sendOrEstimate(hre, ex => withdrawFromAave(ex, sUSDe_ADDRESS, USDe_ADDRESS, 0.1, 0.1), "AaveUsdcVaultProxy")
+}
+
+// noinspection JSUnusedGlobalSymbols
+export default func
+func.tags = ['withdraw-sUSDe-USDe-AAVE']
