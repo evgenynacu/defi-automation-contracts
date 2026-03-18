98147c3f1479cd969c2c32a9b80dd03967c87114
Eugene Nacu <evgeny.nacu@gmail.com>
2026-03-18 11:17:37 +0000

    chore: positions

--- cron/index.ts
+++ cron/index.ts
@@ -220,28 +220,6 @@ async function syncAllNonPTs(syncService: SyncService) {
 		console.error("Error syncing sUSDS/USDT", e)
 	}
 
-	try {
-		await syncService.syncData({
-			type: "morpho-withdraw",
-			from: "0x21F1359b6DD3392d3DC567d005d83B6d017CC60D",
-			vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
-			marketId: "0x29ae8cad946d861464d5e829877245a863a18157c0cde2c3524434dafa34e476"
-		})
-	} catch (e) {
-		console.error("Error syncing sUSDD/USDT", e)
-	}
-
-	try {
-		await syncService.syncData({
-			type: "morpho-withdraw",
-			from: "0x089fa9741628c1A4576F5BA47E02D1180b581e36",
-			vault: "0x5Af8B1e9b34de89a07f6114c2ffB3bABaEdca240",
-			marketId: "0xa5beccdffd156dfe8c0871f143648c512f0a34f37c8a4ae2ff31ebfe944641d1"
-		})
-	} catch (e) {
-		console.error("Error syncing sUSDS/PYUSD", e)
-	}
-
 	console.log("non-PTs synchronized in", (Date.now() - start), "ms")
 }
 
